90e44cc31684483fc909587d1414a99fdc7e6706
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2022-02-10 04:19:14 +0530

    Added a Preloader

---
+++ preloader.js
@@ -0,0 +1,7 @@
+const bodyTag = document.body;
+const loaderContainer = document.getElementById("loader_fixed_container");
+window.onload = function(){
+    loaderContainer.style.opacity = "0";
+    loaderContainer.style.pointerEvents = "none";
+    bodyTag.classList.replace("no_scroll_bar_visible","scroll_bar_visible");
+}
